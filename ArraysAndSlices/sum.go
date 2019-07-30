f09a196fe86371ec4af092235a3d096ff5561146
Jack <jdcorley@gmail.com>
2019-07-30 19:57:04 -0400

    added tail sum total of slice

--- ArraysAndSlices/sum.go
+++ ArraysAndSlices/sum.go
@@ -6,3 +6,25 @@ func Sum(numbers []int) (total int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(tailsToSum ...[]int) []int {
+	var tailSums []int
+	for _, numbers := range tailsToSum {
+		if len(numbers) == 0 {
+			tailSums = append(tailSums, 0)
+		} else {
+			tail := numbers[1:]
+			tailSums = append(tailSums, Sum(tail))
+		}
+	}
+
+	return tailSums
+}
